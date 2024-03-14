f627e4c58c8229e6509fab79cab4cf4292299b56
NuclearReactor314 <145520137+NuclearReactor314@users.noreply.github.com>
2024-03-13 17:18:40 -0700

    Update Node.js

--- Node.js
+++ Node.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 // 创建数据库连接
 const connection = mysql.createConnection({
-  host: 'localhost',      // 数据库主机名
-  user: 'nuclearreactor',  // 数据库用户名
-  password: 'dfufei6340', // 数据库密码
+  host: '76.168.46.4',      // 数据库主机名（手机的 IP 地址）
+  user: 'nuclearreactor',   // 数据库用户名
+  password: 'dfufei6340',   // 数据库密码
   database: 'ChatDatabase'  // 数据库名称
 });
 
@@ -17,13 +17,17 @@ connection.connect((err) => {
   console.log('Connected to database as id ' + connection.threadId);
 });
 
-// 在数据库上执行查询
-connection.query('SELECT * FROM your_table', (err, results, fields) => {
-  if (err) throw err;
-  console.log('Query results:', results);
+// 查询帖子和回帖
+connection.query('SELECT * FROM posts', (err, results, fields) => {
+  if (err) {
+    console.error('Error querying database: ' + err.stack);
+    return;
+  }
+  console.log('Posts:', results);
+  // 在这里处理结果，例如将结果发送到前端显示
 });
 
-// 在关闭连接时进行清理
+// 关闭数据库连接
 connection.end((err) => {
   if (err) {
     console.error('Error closing database connection: ' + err.stack);
